c76df67d8db05a4b6a939abe9e7f74853afb73c0
Pixeloided <pixeloidsemail@gmail.com>
2021-06-17 21:37:09 -0700

    Revamped UI

--- main.py
+++ main.py
@@ -1,42 +1,25 @@
+from tkinter import *
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.keys import Keys
 import time
-import tkinter as tk
 
-root = tk.Tk()
-canvas = tk.Canvas(root, height=600, width=650)
-canvas.pack()
+idplc = '  Kahoot ID'
+nameplc = '  Name of bots'
+countplc = '  Number of bots'
 
-label1 = tk.Label(root, text="Enter Pin: ")
-canvas.create_window(325, 50, window=label1)
-
-gamePinEntry = tk.Entry(root)
-canvas.create_window(325, 75, window=gamePinEntry)
-
-label2 = tk.Label(root, text="Enter Bot Name: ")
-canvas.create_window(325, 100, window=label2)
-botNameEntry = tk.Entry(root)
-canvas.create_window(325, 125, window=botNameEntry)
-
-label3 = tk.Label(root, text="Enter Bot Amount: ")
-canvas.create_window(325, 150, window=label3)
-botAmountEntry = tk.Entry(root)
-canvas.create_window(325, 175, window=botAmountEntry)
-
-
-def runBot():
+def btn_clicked():
     a = 0
     botNumber = 0
-    gamePin = str(gamePinEntry.get())
-    botName = str(botNameEntry.get())
-    botAmount = int(botAmountEntry.get())
+    gamePin = str(kahootId.get())
+    botNamed = str(botName.get())
+    botAmount = int(botCount.get())
 
     while a < botAmount:
         browser = webdriver.Chrome(ChromeDriverManager().install())
         a += 1
         botNumber += 1
-        botNamer = f"{botName}{botNumber}"
+        botNamer = f"{botNamed}{botNumber}"
         browser.get("https://kahoot.it")
         browser.find_element_by_id("game-input").send_keys(gamePin)
         browser.find_element_by_id("game-input").send_keys(Keys.RETURN)
@@ -52,7 +35,128 @@ def runBot():
             browser.find_element_by_xpath('//*[@id="root"]/div[1]/div[2]/div/div[2]/button[2]').click()
 
 
-submit = tk.Button(root, text="Submit", command=runBot)
-canvas.create_window(325, 205, window=submit)
+def eraseId(event=None):
+    if kahootId.get() == idplc:
+        kahootId.delete(0,'end')
+def addId(event=None):
+    if kahootId.get() == '':
+        kahootId.insert(0,idplc)
 
-root.mainloop()
+def eraseName(event=None):
+    if botName.get() == nameplc:
+        botName.delete(0,'end')
+def addName(event=None):
+    if botName.get() == '':
+        botName.insert(0,nameplc)
+
+def eraseCount(event=None):
+    if botCount.get() == countplc:
+        botCount.delete(0,'end')
+def addCount(event=None):
+    if botCount.get() == '':
+        botCount.insert(0,countplc)
+
+
+def addAll():
+    addId()
+    addName()
+    addCount()
+
+window = Tk()
+
+window.geometry("720x512")
+window.configure(bg = "#ffffff")
+canvas = Canvas(
+    window,
+    bg = "#ffffff",
+    height = 512,
+    width = 720,
+    bd = 0,
+    highlightthickness = 0,
+    relief = "ridge")
+canvas.place(x = 0, y = 0)
+
+background_img = PhotoImage(file = f"background.png")
+background = canvas.create_image(
+    184.0, 303.0,
+    image=background_img)
+
+entry0_img = PhotoImage(file = f"img_textBox0.png")
+entry0_bg = canvas.create_image(
+    515.0, 168.5,
+    image = entry0_img)
+
+kahootId = Entry(
+    bd = 0,
+    bg = "#dddddd",
+    highlightthickness = 0)
+
+kahootId.place(
+    x = 332, y = 152,
+    width = 366,
+    height = 31)
+
+canvas.create_text(
+    304.5, 90.0,
+    text = "Kahoot flooder",
+    fill = "#000000",
+    font = ("Calibri", int(57.0)))
+
+entry1_img = PhotoImage(file = f"img_textBox1.png")
+entry1_bg = canvas.create_image(
+    515.0, 227.0,
+    image = entry1_img)
+
+botName = Entry(
+    bd = 0,
+    bg = "#dddddd",
+    highlightthickness = 0)
+
+botName.place(
+    x = 332, y = 211,
+    width = 366,
+    height = 30)
+
+entry2_img = PhotoImage(file = f"img_textBox2.png")
+entry2_bg = canvas.create_image(
+    515.0, 291.5,
+    image = entry2_img)
+
+botCount = Entry(
+    bd = 0,
+    bg = "#dddddd",
+    highlightthickness = 0)
+
+botCount.place(
+    x = 332, y = 275,
+    width = 366,
+    height = 31)
+
+buttonsrc = PhotoImage(file = f"buttonsrc.png")
+b0 = Button(
+    image = buttonsrc,
+    borderwidth = 0,
+    highlightthickness = 0,
+    command = btn_clicked,
+    relief = "flat")
+
+b0.place(
+    x = 332, y = 372,
+    width = 366,
+    height = 32)
+
+addAll()
+
+kahootId.bind('<FocusIn>',eraseId)
+kahootId.bind('<FocusOut>',addId)
+
+botName.bind('<FocusIn>',eraseName)
+botName.bind('<FocusOut>',addName)
+
+botCount.bind('<FocusIn>',eraseCount)
+botCount.bind('<FocusOut>',addCount)
+
+window.title("Kahoot flooder")
+window.iconbitmap("icon.ico")
+window.resizable(False, False)
+window.mainloop()
